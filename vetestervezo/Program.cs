8f6ff27000fba31617e6496f8396cf51e73c0686
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2024-01-18 13:18:42 +0100

    .

--- vetestervezo/Program.cs
+++ vetestervezo/Program.cs
@@ -12,56 +12,102 @@ namespace vetestervezo
 		{
             int agyasokSzama;
             int mely, szel; //ágyások mélysége és szélessége
+            string kezdoValasztas = "";
 
-            #region Adatok bekérése
+            
             //Console.BackgroundColor = ConsoleColor.DarkGreen;
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("\nÜdvözöllek a Vetéstervezőben!\n");
+            Console.BackgroundColor = ConsoleColor.DarkGreen; Console.WriteLine("\n Üdvözöllek a Vetéstervezőben! \n"); Console.ResetColor();
 
-            do
+            Console.WriteLine("\t(T) Tervező");
+            Console.WriteLine("\t(Ú) Új növények feltöltése");
+            Console.WriteLine("\t(K) Kilépés");
+           
+            while (true)
             {
-                Console.Write("\tElőször kérlek add, hogy hány ágyást szeretnél beültetni (szám): ");
-            } while (!int.TryParse(Console.ReadLine(), out agyasokSzama));
 
-            Console.WriteLine("\n\tIlletve kérlek add meg, hogy megkkorák az ágyások");
+                Console.Write("kérlek: ");
+                kezdoValasztas = Console.ReadLine();
 
-            do
+                if (kezdoValasztas == "T")
+                {
+                    break;
+                }
+                if (kezdoValasztas == "Ú")
+                {
+                    break;
+                }
+                if (kezdoValasztas == "K")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ResetColor(); Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("A rendelkezése álló lehetőségek közül válassz"); Console.ResetColor();
+                }
+
+            }
+
+            if (kezdoValasztas == "T")
             {
-                Console.Write("\t  Az ágyások mélysége (szám): ");
-            } while (!int.TryParse(Console.ReadLine(), out mely));
-            do
-            {
-                Console.Write("\t  Az ágyások szélessége (szám): ");
-            } while (!int.TryParse(Console.ReadLine(), out szel));
+                Console.Clear();
+                do
+                {
+                    Console.ResetColor(); Console.Write("\tElőször kérlek add, hogy hány ágyást szeretnél beültetni (szám): "); Console.ResetColor(); Console.ForegroundColor = ConsoleColor.DarkGreen;
+                } while (!int.TryParse(Console.ReadLine(), out agyasokSzama));
 
-            Console.WriteLine("\n");
+                Console.ResetColor(); Console.WriteLine("\n\tIlletve kérlek add meg, hogy megkkorák az ágyások");
+
+                do
+                {
+                    Console.ResetColor();  Console.Write("\t- Az ágyások mélysége (szám): "); Console.ForegroundColor = ConsoleColor.DarkGreen;
+                } while (!int.TryParse(Console.ReadLine(), out mely));
+                do
+                {
+                    Console.ResetColor(); Console.Write("\t- Az ágyások szélessége (szám): "); Console.ForegroundColor = ConsoleColor.DarkGreen;
+                } while (!int.TryParse(Console.ReadLine(), out szel));
+
+                Console.ResetColor(); Console.WriteLine("\n");
 
             /*Console.Write("magasság");
 			mag=Convert.ToInt32(Console.ReadLine());
             Console.Write("szélesség:");
 			szel=Convert.ToInt32(Console.ReadLine());*/
-            #endregion
 
-            Console.WriteLine("asd");
-            Console.WriteLine("\x1b[1masd\x1b[0m");
+            //Console.WriteLine("asd");
+            //Console.WriteLine("\x1b[1masd\x1b[0m");
 
-          
-
-
-            for (int y = 0; y < agyasokSzama; y++)
-			{
-				for (int i = 0; i <= mely; i++)
-				{
-					//Console.WriteLine("asd");
-					for (int j = 1; j < szel; j++)
-					{
+                for (int y = 0; y < agyasokSzama; y++)
+			    {
+				    for (int i = 0; i <= mely; i++)
+				    {
+					    //Console.WriteLine("asd");
+					    for (int j = 1; j < szel; j++)
+					    {
 					
-						Console.Write("*");
-					}
-					Console.WriteLine();
-				}
-				Console.WriteLine("\n");
-			}
+						    Console.Write("*");
+					    }
+					    Console.WriteLine();
+				    }
+				    Console.WriteLine("\n");
+			    }
+
+
+
+
+
+            }
+            if (kezdoValasztas == "K")
+            {
+                Environment.Exit(0);
+            }
+
+ 
+
+
+
+
+
+
 
 
 
