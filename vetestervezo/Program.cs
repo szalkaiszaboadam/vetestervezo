639e48b851484a2ffbe3946bf8dac60fe34359e0
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2024-01-18 11:41:29 +0100

    színezés

--- vetestervezo/Program.cs
+++ vetestervezo/Program.cs
@@ -10,21 +10,59 @@ namespace vetestervezo
 	{
 		static void Main(string[] args)
 		{
-			int h, w;
-			Console.Write("magasság");
-			h=Convert.ToInt32(Console.ReadLine());
+            int agyasokSzama;
+            int mely, szel; //ágyások mélysége és szélessége
+
+            #region Adatok bekérése
+            //Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\nÜdvözöllek a Vetéstervezőben!\n");
+
+            do
+            {
+                Console.Write("\tElőször kérlek add, hogy hány ágyást szeretnél beültetni (szám): ");
+            } while (!int.TryParse(Console.ReadLine(), out agyasokSzama));
+
+            Console.WriteLine("\n\tIlletve kérlek add meg, hogy megkkorák az ágyások");
+
+            do
+            {
+                Console.Write("\t  Az ágyások mélysége (szám): ");
+            } while (!int.TryParse(Console.ReadLine(), out mely));
+            do
+            {
+                Console.Write("\t  Az ágyások szélessége (szám): ");
+            } while (!int.TryParse(Console.ReadLine(), out szel));
+
+            Console.WriteLine("\n");
+
+            /*Console.Write("magasság");
+			mag=Convert.ToInt32(Console.ReadLine());
             Console.Write("szélesség:");
-			w=Convert.ToInt32(Console.ReadLine());
+			szel=Convert.ToInt32(Console.ReadLine());*/
+            #endregion
+
+            Console.WriteLine("asd");
+            Console.WriteLine("\x1b[1masd\x1b[0m");
+
+          
 
 
-			for (int i = 0; i <= h; i++)
+            for (int y = 0; y < agyasokSzama; y++)
 			{
-				for (int j = 1; j < w; j++)
+				for (int i = 0; i <= mely; i++)
 				{
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+					//Console.WriteLine("asd");
+					for (int j = 1; j < szel; j++)
+					{
+					
+						Console.Write("*");
+					}
+					Console.WriteLine();
+				}
+				Console.WriteLine("\n");
+			}
+
 
 
 			Console.ReadKey();
