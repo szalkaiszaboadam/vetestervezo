b8f536db4a45488faacac55ba9d0625271e78837
nagygaga <100219724+nagygaga@users.noreply.github.com>
2024-01-26 08:47:51 +0100

    kesz az adatbazis

---
+++ test/teszt/teszt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Mysqlx.Connection;
+
+namespace teszt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            string database = "balaton";
+            string username = "root";
+            string password = "";
+            string constring = "Server="+server+";"+"Database="+database+";"+"UID="+username+";"+"Password="+password+";";
+            MySqlConnection conn = new MySqlConnection(constring); 
+            conn.Open();    
+            string query = "SELECT * FROM hajok";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();   
+            while (reader.Read()) 
+            {
+                Console.WriteLine(reader["hajoNev"]);
+                Console.WriteLine(reader["hajoOszt"]);
+            }
+            Console.ReadKey();  
+        }
+    }
+}
